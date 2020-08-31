6898a5be21ca9095de8b7a5d9e64a7a923f2fe3b
40thz <chatextazy@gmail.com>
2020-08-31 20:53:31 +0300

    asdf

--- js/Unit/Unit.js
+++
@@ -1,5 +0,0 @@
-const HOUSE__WRAP = document.querySelector(".house");
-const MODAL__MAIN = document.querySelector(".main")
-const MODAL__FLOOR = document.querySelector(".modal__floor__content");
-
-const TEST_MODAL_FLAT = document.querySelectorAll('.flat__content_wrapper');
